7c8330fceb0998aa7f769f5b4313de0d07f46976
Syed <syed@apache.org>
2021-10-06 22:17:12 -0400

    Update script to push to backup registry

--- build_deploy.sh
+++ build_deploy.sh
@@ -24,3 +24,14 @@ skopeo copy --dest-creds "${QUAY_USER}:${QUAY_TOKEN}" \
     "docker-daemon:${IMG}" \
     "docker://${QUAY_IMAGE}:${GIT_HASH}"
 
+# push the image to a backup repository
+BACKUP_BASE_IMG="${BASE_IMG}-backup"
+BACKUP_IMAGE="${BACKUP_URL}/${BACKUP_BASE_IMG}"
+
+skopeo copy --dest-creds "${BACKUP_USER}:${BACKUP_TOKEN}" \
+    "docker-daemon:${IMG}" \
+    "docker://${BACKUP_IMAGE}:latest"
+
+skopeo copy --dest-creds "${BACKUP_USER}:${BACKUP_TOKEN}" \
+    "docker-daemon:${IMG}" \
+    "docker://${BACKUP_IMAGE}:${GIT_HASH}"
