89ed26626c7d16febcaaaeaf7b1b2a32ae88074d
Syed <syed@apache.org>
2021-11-06 06:58:05 -0400

    use HOST_SECRET for username and password for prod

--- run_update.sh
+++ run_update.sh
@@ -1,17 +1,17 @@
 
 case $EXECUTION_TYPE in
     "SCHEMA_VALIDATION")
-        mysql -u $DB_USER -h $DB_HOST -p\'$DB_PASSWORD\' -D $DB_NAME < schemaValidation.sql
+        mysql -u $DB_USER -h $DB_HOST -p$DB_PASSWORD -D $DB_NAME < schemaValidation.sql
     ;;
     "REPLICA_READWRITE_VALIDATION")
-        mysql -u $DB_USER -h $DB_HOST -p"$DB_PASSWORD" -D $DB_NAME < ValidateReadWrite.sql
+        mysql -u $DB_USER -h $DB_HOST -p$DB_PASSWORD -D $DB_NAME < ValidateReadWrite.sql
     ;;
     "UPDATE_DATABASE_SCHEMA")
-        mysql -u $DB_USER -h $DB_HOST -p"$DB_PASSWORD" -D $DB_NAME < update.sql
+        mysql -u $DB_USER -h $DB_HOST -p$DB_PASSWORD -D $DB_NAME < update.sql
     ;;
     "PREWARM_MYSQL")
-        mysql -u $DB_USER -h $DB_HOST -p"$DB_PASSWORD" -D $DB_NAME < generateprewarm.sql > generatedcommands.sql
-        mysql -u $DB_USER -h $DB_HOST -p"$DB_PASSWORD" -D $DB_NAME < generatedcommands.sql > /dev/null
+        mysql -u $DB_USER -h $DB_HOST -p$DB_PASSWORD -D $DB_NAME < generateprewarm.sql > generatedcommands.sql
+        mysql -u $DB_USER -h $DB_HOST -p$DB_PASSWORD -D $DB_NAME < generatedcommands.sql > /dev/null
     ;;
 esac
 
